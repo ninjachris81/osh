e28109baf0773a3c4c3c95943ffac0c66ba4af58
B1 <cbstar@web.de>
2021-11-19 00:19:09 +0100

    deploy

--- deploy/rpi/deploy.sh
+++ deploy/rpi/deploy.sh
@@ -1,4 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+function buildQMqtt {
+   echo "Building QMqtt in $PWD"
+   cd $OSH_ROOT/qmqtt
+   qmake
+   make -j$MAKE_CORE_COUNT
+   make install
+}
+
+function buildOSHLib {
+    cd $OSH_ROOT/$1
+    echo "Building lib in $PWD"
+    qmake
+    #make -j$MAKE_CORE_COUNT
+    #make install
+}
+
+function buildOSHTarget {
+   cd $OSH_ROOT/$1
+   echo "Building target in $PWD"
+   qmake
+   #make -j$MAKE_CORE_COUNT
+}
+
+function printInfos {
+   echo "OSH Root: $OSH_ROOT"
+   echo "Make cores: $MAKE_CORE_COUNT"
+   echo "Building $BUILD_TARGET..."
+}
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 
 if [ $# -eq 0 ]
   then
@@ -6,4 +40,35 @@ if [ $# -eq 0 ]
 	exit 1
 fi
 
+RPI_MODEL="$(tr -d '\0' </proc/device-tree/model)"
+case $RPI_MODEL in
+   *Zero*		)
+      MAKE_CORE_COUNT=2
+      ;;
+   *			)
+      MAKE_CORE_COUNT=4
+      ;;
+esac
 
+OSH_ROOT="$(dirname "$(cd .. && pwd)")"
+BUILD_TARGET="Unknown"
+
+case "$1" in
+   CoreService		)
+      BUILD_TARGET=$1
+      printInfos
+      buildQMqtt
+      buildOSHLib "Core"
+      buildOSHLib "QMqttCommunicationManager"
+      buildOSHLib "CoreServer"
+      buildOSHTarget "CoreService"
+      ;;
+   *			)
+      ;;
+esac
+
+if [ "$BUILD_TARGET" = "Unknown" ]
+   then
+      echo "Invalid build target"
+         exit 2
+fi
