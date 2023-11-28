f3a2ab7f76fee8654c3b2e54ba1acab5f321aa75
ninjachris81 <cbstar@web.de>
2023-11-29 00:34:01 +0100

    test

--- RS485EnergyMeterController/rs485energymetercontroller.cpp
+++ RS485EnergyMeterController/rs485energymetercontroller.cpp
@@ -188,8 +188,8 @@ QVariant RS485EnergyMeterController::parseValue(QVector<quint16> values, QVarian
     if (twoByte && values.size() == 2) {
         quint32 result;
         quint16* result_arr = (quint16*)& result;
-        result_arr[0] = values.at(0);
-        result_arr[1] = values.at(1);
+        result_arr[0] = values.at(1);
+        result_arr[1] = values.at(0);
         tempValue = result;
     } else if (values.size() == 1){
         tempValue = values.at(0);
