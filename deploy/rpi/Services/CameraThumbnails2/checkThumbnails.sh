7822bad4fc01ef5c7e480cc635a3e31b8b6968ce
ninjachris81 <cbstar@web.de>
2025-04-27 11:27:56 +0200

    dirs

--- deploy/rpi/Services/CameraThumbnails2/checkThumbnails.sh
+++ deploy/rpi/Services/CameraThumbnails2/checkThumbnails.sh
@@ -5,4 +5,4 @@ VIDEO_DIR=/var/ftp_data/videos
 MAX_DAYS_IMAGE=7
 MAX_DAYS_VIDEO=14
 
-../../../../scripts/checkThumbnails.sh $IMAGE_DIR $VIDEO_DIR $MAX_DAYS_IMAGE $MAX_DAYS_VIDEO
+scripts/checkThumbnails.sh $IMAGE_DIR $VIDEO_DIR $MAX_DAYS_IMAGE $MAX_DAYS_VIDEO
